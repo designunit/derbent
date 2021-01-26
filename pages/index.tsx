8b0be7ef29956c6153659c747dbbcaaf6aa527e7
Roman Timashev <roman@tmshv.ru>
2021-01-26 22:54:54 +0300

    + metrika

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,22 @@
 import Head from 'next/head'
+import { YMetrika } from '@/components/YMetrika'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const metrika = process.env.NEXT_PUBLIC_YANDEX_METRIKA
+
   return (
     <div className={styles.container}>
       <Head>
         <title>мойдербент.рф</title>
         <link rel="icon" href="/favicon.ico" />
+
+        {!metrika ? null : (
+          <YMetrika
+            number={metrika}
+            mode={'script'}
+          />
+        )}
       </Head>
 
       <main className={styles.main}>
