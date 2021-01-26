5f799ae8464f34e9e2d8e448019dd10e5b89b395
Roman Timashev <roman@tmshv.ru>
2021-01-26 23:42:52 +0300

    indent 4

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,37 +3,37 @@ import { YMetrika } from '@/components/YMetrika'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  const metrika = process.env.NEXT_PUBLIC_YANDEX_METRIKA
+    const metrika = process.env.NEXT_PUBLIC_YANDEX_METRIKA
 
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>мойдербент.рф</title>
-        <link rel="icon" href="/favicon.ico" />
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>мойдербент.рф</title>
+                <link rel="icon" href="/favicon.ico" />
 
-        {!metrika ? null : (
-          <YMetrika
-            number={metrika}
-            mode={'script'}
-          />
-        )}
-      </Head>
+                {!metrika ? null : (
+                    <YMetrika
+                        number={metrika}
+                        mode={'script'}
+                    />
+                )}
+            </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          мойдербент.рф
-        </h1>
-      </main>
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    мойдербент.рф
+                </h1>
+            </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://unit4.io"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          design::unit
-        </a>
-      </footer>
-    </div>
-  )
+            <footer className={styles.footer}>
+                <a
+                    href="https://unit4.io"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    design::unit
+                </a>
+            </footer>
+        </div>
+    )
 }
